5c3bdf3fcc74f0c0630c411c2104138812785e9e
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2015-03-08 07:38:59 +0200

    adding column width class

--- code/BootstrapTextField.php
+++ code/BootstrapTextField.php
@@ -6,17 +6,39 @@
  * @author Patrick Chitovoro
  * @copyright (c) 2014, Chito Systems
  */
-class BootstrapTextField extends TextField {
+class BootstrapTextField extends TextField
+{
 
-    public function Field($properties = array()) {
+    protected $columnWidth;
+    /**
+     * Returns an input field, class="text" and type="text" with an optional maxlength
+     *
+     * @param $name
+     * @param null $title
+     * @param string $value
+     * @param null $maxLength
+     * @param null $form
+     * @param null $columnWidth
+     */
+    public function __construct($name, $title = null, $value = '', $columnWidth=null ,$maxLength = null, $form = null)
+    {
+        $this->maxLength = $maxLength;
+        $this->columnWidth = $columnWidth;
+
+        parent::__construct($name, $title, $value, $form);
+    }
+
+    public function Field($properties = array())
+    {
 
         return parent::Field($properties);
     }
 
     protected $extraClasses = array('form-control', 'field', 'text');
 
-    public function extraClasses() {
-        $classes = array('field', 'form-control', parent::extraClasses());
+    public function extraClasses()
+    {
+        $classes = array($this->columnWidth,'field', 'form-control', parent::extraClasses());
         return implode(' ', $classes);
     }
 
