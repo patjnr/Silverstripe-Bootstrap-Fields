b2e477433614c47c41ee71fc5b9e2bd99f8dbe91
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2015-03-08 18:41:34 +0200

    adding column width class

--- _config.php
+++ _config.php
@@ -5,6 +5,4 @@
 
 define('BOOTSTRAP_FIELDS__DIR', basename(dirname(__FILE__)));
 define('BOOTSTRAP_FIELDS_BOWER_DIR', BOOTSTRAP_FIELDS__DIR . "/bower_components/");
-define('BOOTSTRAP_DIR', BOOTSTRAP_FIELDS_BOWER_DIR . "bootstrap/dist/");
-
 define('DATETIMEPICKER_DIR', BOOTSTRAP_FIELDS_BOWER_DIR . "eonasdan-bootstrap-datetimepicker/build/");
